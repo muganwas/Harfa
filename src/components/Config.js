b145a54febd424c1a3164e573150799a465b658d
Muganwa <stevenmuganwa@gmail.com>
2020-09-28 02:24:42 +0300

    Update Config.js

--- src/components/Config.js
+++ src/components/Config.js
@@ -2,7 +2,8 @@ import io from 'socket.io-client';
 import { 
   BASE_URL_CLOUD,
   BASE_URL_LOCAL,
-  WEB_CLIENT_ID,
+  BASE_URL_LOCAL_OUT,
+  //WEB_CLIENT_ID,
   BASE_URL_LOCAL_ALT,
   FIREBASE_API_KEY, 
   FIREBASE_APP_ID, 
@@ -12,10 +13,10 @@ import {
   FIREBASE_STORAGE_BUCKET,
   FIREBASE_MESSAGING_SENDER_ID
 } from 'react-native-dotenv';
-//local-alt
+//local alt --
 export default class Config {
-  static clientId = WEB_CLIENT_ID
-  static baseURL = BASE_URL_LOCAL_ALT
+  //static clientId = WEB_CLIENT_ID
+  static baseURL = BASE_URL_CLOUD
   static apiKey = FIREBASE_API_KEY
   static authDomain = FIREBASE_AUTH_DOMAIN
   static databaseURL = FIREBASE_DATABASE_URL
